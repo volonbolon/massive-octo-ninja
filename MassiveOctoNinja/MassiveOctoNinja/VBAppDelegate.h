bb127338c91875d0bc59299a559e601a2e0f8b4b
Ariel Rodriguez <ariel@volonbolon.net>
2014-01-23 17:34:16 -0300

    Add Xcode project

---
+++ MassiveOctoNinja/MassiveOctoNinja/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  MassiveOctoNinja
+//
+//  Created by Ariel Rodriguez on 1/23/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MassiveOctoNinja/MassiveOctoNinja/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  MassiveOctoNinja
+//
+//  Created by Ariel Rodriguez on 1/23/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
