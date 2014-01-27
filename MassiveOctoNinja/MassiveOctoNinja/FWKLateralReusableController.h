4f8b9ff1b41f272a9910c39893b37b8bca6e783c
Ariel Rodriguez <ariel@volonbolon.net>
2014-01-26 21:39:19 -0300

    Add documentation to FWKLateralReusableController

--- MassiveOctoNinja/MassiveOctoNinja/FWKLateralReusableController.h
+++ MassiveOctoNinja/MassiveOctoNinja/FWKLateralReusableController.h
@@ -8,10 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Protocol that all the controllers used in the lateral navigator needs to implement.
+ */
 @protocol FWKLateralReusableController <NSObject>
 @required
+/**
+ We save the identifier associated with the controller class to perform some instrospection
+ */
 @property (nonatomic, strong) NSString *identifier;
 
+/**
+ Called right before adding the view to the container view. Clients should clean pictures and make sure the controller is ready to be reused after returning from this method.
+ */
 @required
 - (void)prepareForReuse;
 @end
