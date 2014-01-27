1488d25ef60d0f2fc8a34dec893ee4ec25e105d8
Ariel Rodriguez <ariel@volonbolon.net>
2014-01-26 21:41:05 -0300

    Add documentation to FWKReusableCacheInformation

--- MassiveOctoNinja/MassiveOctoNinja/FWKReusableCacheInformation.h
+++ MassiveOctoNinja/MassiveOctoNinja/FWKReusableCacheInformation.h
@@ -8,6 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Used internally to save information about the classes that are registered for the lateral navigator controller. 
+ This is just a convenience class, and since it is only a matter of implementation, should be treated as private.
+ */
 @interface FWKReusableCacheInformation : NSObject
 @property (nonatomic, strong) Class controllerClass;
 @property (nonatomic, strong) NSMutableArray *cache;
